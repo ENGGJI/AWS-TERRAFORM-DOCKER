6362a91f2241fc2d83beeea56f868e45d9d9cde9
Ashutosh Upadhyay <itsashutosh1998@gmail.com>
2025-03-26 07:01:07 +0530

    Create main.tf
    The primary configuration file that sets up the AWS provider and Terraform backend.

---
+++ main.tf
@@ -0,0 +1,21 @@
+# main.tf
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
